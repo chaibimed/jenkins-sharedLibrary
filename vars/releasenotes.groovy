d5e92d221fd5d9ce1dc20cf299418e5b5670c798
Chaibi Med <chaibi.med@hotmail.fr>
2023-02-06 20:03:38 +0100

    Update releasenotes.groovy

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,9 +1,10 @@
 import java.io.*;
 import groovy.io.*;
 
+@NonCPS
 def call(Map config=[:]) {
   def dir = new File(pwd());
-  @NonCPS
+ 
   new File(dir.path + '\\releasenotes.txt').withWriter('utf-8')
   {
     writer -> dir.eachFileRecurse(FileType.ANY){ file ->
