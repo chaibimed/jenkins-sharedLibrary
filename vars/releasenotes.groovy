431f3ff837e66f1958243c5a35d0b9d019754ba7
Chaibi Med <chaibi.med@hotmail.fr>
2023-02-06 19:35:08 +0100

    Update releasenotes.groovy

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -5,15 +5,15 @@ def call(Map config=[:]) {
   def dir = new File(pwd());
 
   new File(dir.path + '/releasenotes.txt').withWriter('utf-8')
-
-  writer -> dir.eachFileRecurse(FileType.ANY){ file ->
-     if(file.isDirectory()){
-        writer.writeLine(file.name);
-     }
-     else
-     {
-        writer.writeLine('\t' + file.name +'\t' + file.length());
-     }
-
+  {
+    writer -> dir.eachFileRecurse(FileType.ANY){ file ->
+       if(file.isDirectory()){
+          writer.writeLine(file.name);
+       }
+       else
+       {
+          writer.writeLine('\t' + file.name +'\t' + file.length());
+       }
+    }
   }
 }
